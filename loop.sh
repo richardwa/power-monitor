e0600889c420db14ec6f10cd659d5ee83e7a857c
Richard <rwang83@gmail.com>
2024-11-21 17:31:02 -0500

    update

---
+++ loop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+COMMAND='mosquitto_sub -v -t "#"'
+
+while true; do
+    current_time=$(date +%s)
+    midnight=$(date -d "tomorrow 00:00:00" +%s)
+    seconds_remaining=$((midnight - current_time))
+    log_file="/data/power-$(date +%Y-%m-%d).log"
+
+    timeout seconds_remaining $COMMAND >> $log_file
+    sleep 10
+done
--- start.sh
+++ start.sh
@@ -3,7 +3,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo start power-monitor
 
-mosquitto -d -c /app/mqtt.conf 
-python /app/mqtt_logger.py
+mosquitto -d -c $DIR/mqtt.conf 
+$DIR/loop.sh
 
-echo started
+echo stopped
+
